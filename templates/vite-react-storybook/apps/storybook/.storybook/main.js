b4e4cc643194c10cc627596418511e0d4b226c85
Dion Ridley <dridley@gmail.com>
2025-07-29 18:28:16 -0700

    Storybook fixes (#4)
    * Update Storybook application
    * Minor updates to support storybook directory structure

---
+++ templates/vite-react-storybook/apps/storybook/.storybook/main.js
@@ -0,0 +1,23 @@
+
+
+/** @type { import('@storybook/react-vite').StorybookConfig } */
+const config = {
+  "stories": [
+    "../src/**/*.mdx",
+    "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)",
+    // Template Additions
+    "../stories/**/*.mdx",
+    "../stories/**/*.stories.@(js|jsx|mjs|ts|tsx)"
+  ],
+  "addons": [
+    "@chromatic-com/storybook",
+    "@storybook/addon-docs",
+    "@storybook/addon-a11y",
+    "@storybook/addon-vitest"
+  ],
+  "framework": {
+    "name": "@storybook/react-vite",
+    "options": {}
+  }
+};
+export default config;
